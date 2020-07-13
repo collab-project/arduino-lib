9049303729168967a9613b27c20574db7acdcd47
Thijs Triemstra <info@collab.nl>
2020-07-13 02:19:55 +0200

    add non-mux version

---
+++ Capacitive_SoilSensor/Capacitive_SoilSensor.cpp
@@ -0,0 +1,22 @@
+/*
+  Capacitive_SoilSensor.cpp - Read Capacitive Soil Moisture Sensor.
+*/
+#include "Capacitive_SoilSensor.h"
+
+Capacitive_SoilSensor::Capacitive_SoilSensor(int pin, int wet, int dry) {
+  _pin = pin;
+  _wet = wet;
+  _dry = dry;
+}
+
+int Capacitive_SoilSensor::measure() {
+  return 0;
+}
+
+int Capacitive_SoilSensor::measurePercentage() {
+  // read the input
+  int data = measure();
+
+  // map to percentage
+  return _max(map(data, _wet, _dry, 100, 0), 0);
+}
---
+++ Capacitive_SoilSensor/Capacitive_SoilSensor.h
@@ -0,0 +1,22 @@
+/*
+  Capacitive_SoilSensor_Mux.h - Read Capacitive Soil Moisture Sensor.
+*/
+#ifndef Capacitive_SoilSensor_h
+#define Capacitive_SoilSensor_h
+
+#include "Arduino.h"
+
+class Capacitive_SoilSensor
+{
+  public:
+    Capacitive_SoilSensor(int pin, int wet, int dry);
+    int measure();
+    int measurePercentage();
+
+  private:
+    int _pin;
+    int _wet;
+    int _dry;
+};
+
+#endif
