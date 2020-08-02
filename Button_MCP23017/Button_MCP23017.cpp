7ec5f9195c197b9ba7fb54cc7d6ea43f71861b47
Thijs Triemstra <info@collab.nl>
2020-08-02 21:36:16 +0200

    add mcp23017 button

---
+++ Button_MCP23017/Button_MCP23017.cpp
@@ -0,0 +1,43 @@
+/*
+  Button_MCP23017.cpp - Control MCP23017 connected button.
+*/
+
+#include "Button_MCP23017.h"
+
+Button_MCP23017::Button_MCP23017(int btn_pin, MCP_PORT mcp_port, MultiPlexer_MCP23017* mcp, Method callback) {
+  _btnPin = btn_pin;
+  _mcp = mcp;
+  _port = mcp_port;
+  _callback = callback;
+  _lastBtnState = LOW;
+}
+
+void Button_MCP23017::begin() {
+}
+
+void Button_MCP23017::loop() {
+  read();
+}
+
+void Button_MCP23017::read() {
+  int buttonReading = _mcp->getPin(_btnPin, _port);
+
+  // if the switch changed, due to noise or pressing
+  if (buttonReading != _lastBtnState) {
+    // reset the debouncing timer
+    _lastDebounceTime = millis();
+  }
+  if ((millis() - _lastDebounceTime) > _debounceDelay) {
+    // if the button state has changed
+    if (buttonReading != _currentBtnState) {
+      _currentBtnState = buttonReading;
+
+      // only toggle if the new button state is HIGH
+      if (_currentBtnState == HIGH) {
+        _callback.callback();
+      }
+    }
+  }
+  // save the switch reading
+  _lastBtnState = buttonReading;
+}
---
+++ Button_MCP23017/Button_MCP23017.h
@@ -0,0 +1,35 @@
+/*
+  Button_MCP23017.cpp - Control MCP23017 connected button.
+*/
+#ifndef Button_MCP23017_h
+#define Button_MCP23017_h
+
+#include <Arduino.h>
+#include <Method.h>
+#include <MultiPlexer_MCP23017.h>
+
+class Button_MCP23017
+{
+  public:
+    Button_MCP23017(
+      int btn_pin,
+      MCP_PORT mcp_port,
+      MultiPlexer_MCP23017* mcp,
+      Method callback
+    );
+    void loop();
+    void begin();
+    void read();
+
+  private:
+    int _btnPin;
+    int _currentBtnState;
+    int _lastBtnState;
+    MCP_PORT _port;
+    Method _callback;
+    MultiPlexer_MCP23017* _mcp;
+    unsigned long _lastDebounceTime = 0;
+    unsigned long _debounceDelay = 50;
+};
+
+#endif
