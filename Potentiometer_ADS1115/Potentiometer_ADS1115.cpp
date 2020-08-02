b16b25e2f3dc039e9a3c0cfde5d14d9e00e9a257
Thijs Triemstra <info@collab.nl>
2020-08-02 23:07:05 +0200

    add ads1115 potentiometer

---
+++ Potentiometer_ADS1115/Potentiometer_ADS1115.cpp
@@ -0,0 +1,35 @@
+/*
+  Potentiometer_ADS1115.cpp - Control potentiometer using MCP23017.
+*/
+
+#include "Potentiometer_ADS1115.h"
+
+Potentiometer_ADS1115::Potentiometer_ADS1115(
+    int pin,
+    MultiPlexer_ADS1115* adc,
+    Method callback
+) {
+  _pin = pin;
+  _adc = adc;
+  _callback = callback;
+}
+
+void Potentiometer_ADS1115::begin() {
+}
+
+void Potentiometer_ADS1115::loop() {
+  read();
+}
+
+void Potentiometer_ADS1115::read() {
+  if (_pin == 0) {
+    _val = _adc->readChannel0();
+  } else if (_pin == 1) {
+    _val = _adc->readChannel1();
+  } else if (_pin == 2) {
+    _val = _adc->readChannel2();
+  } else if (_pin == 3) {
+    _val = _adc->readChannel3();
+  }
+  //_callback.callback();
+}
---
+++ Potentiometer_ADS1115/Potentiometer_ADS1115.h
@@ -0,0 +1,30 @@
+/*
+  Potentiometer_ADS1115.cpp - Control potentiometer using MCP23017.
+*/
+#ifndef Potentiometer_ADS1115_h
+#define Potentiometer_ADS1115_h
+
+#include <Arduino.h>
+#include <Method.h>
+#include <MultiPlexer_ADS1115.h>
+
+class Potentiometer_ADS1115
+{
+  public:
+    Potentiometer_ADS1115(
+      int pin,
+      MultiPlexer_ADS1115* adc,
+      Method callback
+    );
+    void loop();
+    void begin();
+    void read();
+
+  private:
+    int _pin;
+    float _val;
+    Method _callback;
+    MultiPlexer_ADS1115* _adc;
+};
+
+#endif
