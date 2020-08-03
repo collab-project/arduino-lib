db5f0168d28153c4d0c46627d2d8c39b14584f51
Thijs Triemstra <info@collab.nl>
2020-08-03 14:03:59 +0200

    add attachCallbackIntArg

--- Method/Method.cpp
+++ Method/Method.cpp
@@ -1,20 +1,24 @@
 #include "Method.h"
 
+Method::Method() {}
 
-// Method
-Method::Method()
-{
-}
-
-void Method::attachCallback(const Functor0 & callback)
-{
+void Method::attachCallback(const Functor0 & callback) {
   callback_ = callback;
 }
 
-void Method::callback()
-{
-  if (callback_)
-  {
+void Method::attachCallbackIntArg(const Functor1<int> & callback) {
+  callbackIntArg_ = callback;
+}
+
+void Method::callback() {
+  if (callback_) {
     callback_();
   }
 }
+
+void Method::callbackIntArg(int val) {
+  if (callbackIntArg_) {
+    callbackIntArg_(val);
+  }
+}
+
--- Method/Method.h
+++ Method/Method.h
@@ -7,10 +7,13 @@ class Method
   public:
     Method();
     void attachCallback(const Functor0 & callback);
+    void attachCallbackIntArg(const Functor1<int> & callback);
     void callback();
+    void callbackIntArg(int val);
 
   protected:
     Functor0 callback_;
+    Functor1<int> callbackIntArg_;
 };
 
 #endif
