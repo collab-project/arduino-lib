2400ddde48fdb844c42e668b8a940559dbadc9e9
Thijs Triemstra <info@collab.nl>
2021-04-26 12:23:44 +0200

    doc update

--- YX5300_AudioPlayer/YX5300_AudioPlayer.cpp
+++ YX5300_AudioPlayer/YX5300_AudioPlayer.cpp
@@ -14,40 +14,65 @@ YX5300_AudioPlayer::YX5300_AudioPlayer(
 ) {
   _volume = volume;
 
-  // MP3 player serial stream for comms
   _stream = new SoftwareSerial(rx_pin, tx_pin);
   _player = new MD_YX5300(*_stream);
 }
 
 void cbResponse(const MD_YX5300::cbData *status) {
+  Serial.print(F("MD_YX5300 - "));
+
   switch (status->code) {
-    case MD_YX5300::STS_OK:         Serial.print(F("STS_OK"));         break;
-    case MD_YX5300::STS_TIMEOUT:    Serial.print(F("STS_TIMEOUT"));    break;
-    case MD_YX5300::STS_VERSION:    Serial.print(F("STS_VERSION"));    break;
-    case MD_YX5300::STS_CHECKSUM:   Serial.print(F("STS_CHECKSUM"));    break;
+    case MD_YX5300::STS_OK:
+      Serial.println(F("STS_OK"));
+      break;
+
+    case MD_YX5300::STS_TIMEOUT:
+      Serial.println(F("STS_TIMEOUT"));
+      break;
+
+    case MD_YX5300::STS_VERSION:
+      Serial.println(F("STS_VERSION"));
+      break;
+
+    case MD_YX5300::STS_CHECKSUM:
+      Serial.println(F("STS_CHECKSUM"));
+      break;
+
     case MD_YX5300::STS_TF_INSERT:
       // card has been inserted
-      Serial.println(F("STS_TF_INSERT"));
+      Serial.println(F("Card inserted."));
       break;
 
     case MD_YX5300::STS_TF_REMOVE:
       // card has been removed
-      Serial.println(F("STS_TF_REMOVE"));
+      Serial.println(F("Card removed."));
       break;
 
     case MD_YX5300::STS_ACK_OK:
       // ignore
-      //Serial.print(F("STS_ACK_OK"));
+      //Serial.println(F("STS_ACK_OK"));
       break;
 
     case MD_YX5300::STS_FILE_END:
       Serial.println(F("File ended."));
       break;
 
-    case MD_YX5300::STS_ERR_FILE:   Serial.print(F("STS_ERR_FILE"));   break;
-    case MD_YX5300::STS_INIT:       Serial.print(F("STS_INIT"));       break;
-    case MD_YX5300::STS_STATUS:     Serial.print(F("STS_STATUS"));     break;
-    case MD_YX5300::STS_EQUALIZER:  Serial.print(F("STS_EQUALIZER"));  break;
+    case MD_YX5300::STS_ERR_FILE:
+      Serial.println(F("STS_ERR_FILE"));
+      break;
+
+    case MD_YX5300::STS_INIT:
+      Serial.println(F("STS_INIT"));
+      break;
+
+    case MD_YX5300::STS_STATUS:
+      Serial.println(F("STS_STATUS"));
+      break;
+
+    case MD_YX5300::STS_EQUALIZER:
+      Serial.println(F("STS_EQUALIZER"));
+      break;
+
     case MD_YX5300::STS_VOLUME:
       Serial.print(F("Volume:\t\t"));
       Serial.println(status->data);
@@ -63,8 +88,14 @@ void cbResponse(const MD_YX5300::cbData *status) {
       Serial.println(status->data);
       break;
 
-    case MD_YX5300::STS_FLDR_FILES: Serial.print(F("STS_FLDR_FILES")); break;
-    case MD_YX5300::STS_TOT_FLDR:   Serial.print(F("STS_TOT_FLDR"));   break;
+    case MD_YX5300::STS_FLDR_FILES:
+      Serial.println(F("STS_FLDR_FILES"));
+      break;
+
+    case MD_YX5300::STS_TOT_FLDR:
+      Serial.println(F("STS_TOT_FLDR"));
+      break;
+
     default:
       Serial.print(F("STS_??? 0x"));
       Serial.println(status->code, HEX);
@@ -91,11 +122,13 @@ void YX5300_AudioPlayer::query() {
   _player->queryFilesCount();
   _player->queryVolume();
 
-  Serial.print(F("Max volume:\t"));
+  Serial.print(F("MD_YX5300 - Max volume:\t"));
   Serial.println(_player->volumeMax());
 }
 
 void YX5300_AudioPlayer::stop() {
+  Serial.println(F("MD_YX5300 - Stop playback"));
+
   _player->playStop();
 }
 
@@ -104,6 +137,8 @@ void YX5300_AudioPlayer::playFolderRepeat(uint8_t folder) {
 }
 
 void YX5300_AudioPlayer::nextTrack() {
+  Serial.println(F("MD_YX5300 - Play next track"));
+
   _player->playNext();
 }
 
