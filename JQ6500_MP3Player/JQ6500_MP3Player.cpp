b451bf377f6155cca6a4ddfc0ce937f3539e4fe5
Thijs Triemstra <info@collab.nl>
2021-05-25 22:39:24 +0200

    extend implementation

--- JQ6500_MP3Player/JQ6500_MP3Player.cpp
+++ JQ6500_MP3Player/JQ6500_MP3Player.cpp
@@ -7,36 +7,82 @@
 
 #include <JQ6500_MP3Player.h>
 
-JQ6500_MP3Player::JQ6500_MP3Player(short rx_pin, short tx_pin, int volume, long speed) {
+JQ6500_MP3Player::JQ6500_MP3Player(
+  short rx_pin,
+  short tx_pin,
+  int volume,
+  int source,
+  long baud_rate
+) {
   _player = new JQ6500_Serial(rx_pin, tx_pin);
-  _speed = speed;
   _volume = volume;
+  _source = source;
+  _baudRate = baud_rate;
 }
 
 void JQ6500_MP3Player::begin() {
-  _player->begin(_speed);
+  _player->begin(_baudRate);
   _player->reset();
-  _player->setSource(MP3_SRC_SDCARD);
-  _player->setVolume(_volume);
+
+  setSource(_source);
+  setVolume(_volume);
+
   _player->setLoopMode(MP3_LOOP_ALL);
 
-  unsigned int totalLocalFiles = _player->countFiles(MP3_SRC_BUILTIN);
-  Serial.print("Local files: ");
+  unsigned int totalLocalFiles = getTotalFiles(MP3_SRC_BUILTIN);
+  Serial.print("Local files:\t");
   Serial.println(totalLocalFiles);
 
-  unsigned int totalSDFiles = _player->countFiles(MP3_SRC_SDCARD);
-  Serial.print("SD-card files: ");
+  unsigned int totalSDFiles = getTotalFiles(MP3_SRC_SDCARD);
+  Serial.print("SD-card files:\t");
   Serial.println(totalSDFiles);
 
-  Serial.print("Volume: ");
-  Serial.println(_player->getVolume());
+  Serial.print("Volume:\t\t");
+  Serial.println(getVolume());
 
-  Serial.print("Loop mode: ");
+  Serial.print("Loop mode:\t");
   Serial.println(_player->getLoopMode());
 
-  if (_player->getStatus() != MP3_STATUS_PLAYING) {
+  if (getStatus() != MP3_STATUS_PLAYING) {
     _player->next();
   }
+}
+
+void JQ6500_MP3Player::playSpecific(unsigned int folder, unsigned int track) {
+  _player->playFileNumberInFolderNumber(folder, track);
+}
+
+int JQ6500_MP3Player::getVolume() {
+  _volume = _player->getVolume();
+  return _volume;
+}
+
+void JQ6500_MP3Player::setVolume(int volume) {
+  _volume = volume;
+  _player->setVolume(_volume);
+}
+
+void JQ6500_MP3Player::setSource(int source) {
+  _source = source;
+  _player->setSource(_source);
+}
+
+int JQ6500_MP3Player::getEqualizer() {
+  _equalizerMode = _player->getEqualizer();
+  return _equalizerMode;
+}
+
+void JQ6500_MP3Player::setEqualizer(int equalizerMode) {
+  _equalizerMode = equalizerMode;
+  _player->setEqualizer(_equalizerMode);
+}
+
+unsigned int JQ6500_MP3Player::getTotalFiles(int source) {
+  return _player->countFiles(source);
+}
+
+byte JQ6500_MP3Player::getStatus() {
+  return _player->getStatus();
 }
 
 void JQ6500_MP3Player::loop() {
--- JQ6500_MP3Player/JQ6500_MP3Player.h
+++ JQ6500_MP3Player/JQ6500_MP3Player.h
@@ -14,14 +14,31 @@
 class JQ6500_MP3Player
 {
   public:
-    JQ6500_MP3Player(short rx_pin, short tx_pin, int volume = 10, long speed = 9600);
+    JQ6500_MP3Player(
+      short rx_pin,
+      short tx_pin,
+      int volume = 10,
+      int source = MP3_SRC_SDCARD,
+      long baud_rate = 9600
+    );
     void begin();
     void loop();
 
+    byte getStatus();
+    int getVolume();
+    void setVolume(int volume);
+    void setSource(int source);
+    int getEqualizer();
+    void setEqualizer(int equalizerMode);
+    unsigned int getTotalFiles(int source);
+    void playSpecific(unsigned int folder, unsigned int track);
+
   private:
     JQ6500_Serial* _player;
-    long _speed;
+    long _baudRate;
     int _volume;
+    int _source;
+    int _equalizerMode;
 };
 
 #endif
