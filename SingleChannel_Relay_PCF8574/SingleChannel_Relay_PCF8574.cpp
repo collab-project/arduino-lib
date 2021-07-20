93aaa9fc9df3806eb52f4b5a2ceaa83bfa7a7abc
Thijs Triemstra <info@collab.nl>
2021-07-20 18:36:46 +0200

    use multiple pins in order to have enough power to trigger the relay (on d1 mini relay shield)

--- SingleChannel_Relay_PCF8574/SingleChannel_Relay_PCF8574.cpp
+++ SingleChannel_Relay_PCF8574/SingleChannel_Relay_PCF8574.cpp
@@ -5,33 +5,45 @@
 #include "SingleChannel_Relay_PCF8574.h"
 
 SingleChannel_Relay_PCF8574::SingleChannel_Relay_PCF8574(
-  MultiPlexer_PCF8574* pcf, int relay_pin, bool normally_open) {
+  MultiPlexer_PCF8574* pcf, int relay_pin1, int relay_pin2, bool normally_open) {
   _expander = pcf;
-  _relayPin = relay_pin;
+  _relayPin1 = relay_pin1;
+  _relayPin2 = relay_pin2;
   _normallyOpen = normally_open;
 }
 
 void SingleChannel_Relay_PCF8574::begin() {
-  _expander->pinMode(_relayPin, OUTPUT, LOW);
 }
 
 void SingleChannel_Relay_PCF8574::loop() {
 }
 
 void SingleChannel_Relay_PCF8574::start() {
+  int val;
   if (_normallyOpen == true) {
     // Normally Open configuration, send LOW signal to start current flow
-    _expander->digitalWrite(_relayPin, LOW);
+    val = LOW;
   } else {
-    _expander->digitalWrite(_relayPin, HIGH);
+    val = HIGH;
   }
+  write(val);
 }
 
 void SingleChannel_Relay_PCF8574::stop() {
+  int val;
   if (_normallyOpen == true) {
     // Normally Open configuration, send HIGH signal stop current flow
-    _expander->digitalWrite(_relayPin, HIGH);
+    val = HIGH;
   } else {
-    _expander->digitalWrite(_relayPin, LOW);
+    val = LOW;
+  }
+  write(val);
+}
+
+void SingleChannel_Relay_PCF8574::write(int val) {
+  _expander->digitalWrite(_relayPin1, val);
+
+  if (_relayPin2 != -1) {
+    _expander->digitalWrite(_relayPin2, val);
   }
 }
--- SingleChannel_Relay_PCF8574/SingleChannel_Relay_PCF8574.h
+++ SingleChannel_Relay_PCF8574/SingleChannel_Relay_PCF8574.h
@@ -12,16 +12,24 @@ class SingleChannel_Relay_PCF8574
 {
   public:
     SingleChannel_Relay_PCF8574() {};
-    SingleChannel_Relay_PCF8574(MultiPlexer_PCF8574* pcf, int relay_pin, bool normally_open = true);
+    SingleChannel_Relay_PCF8574(
+      MultiPlexer_PCF8574* pcf,
+      int relay_pin,
+      int relay_pin2 = -1,
+      bool normally_open = true
+    );
     void begin();
     void loop();
     void start();
     void stop();
 
   private:
-    int _relayPin;
+    int _relayPin1;
+    int _relayPin2;
     bool _normallyOpen;
     MultiPlexer_PCF8574* _expander;
+
+    void write(int val);
 };
 
 #endif
