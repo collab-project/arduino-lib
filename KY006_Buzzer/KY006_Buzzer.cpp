15b81808f0e375b471504cc71ecc3d6506bbcf84
Thijs Triemstra <info@collab.nl>
2021-07-21 23:08:26 +0200

    add KY-006 buzzer

---
+++ KY006_Buzzer/KY006_Buzzer.cpp
@@ -0,0 +1,21 @@
+/*  Copyright (c) 2021, Collab
+ *  All rights reserved
+*/
+/*
+  KY006_Buzzer.cpp - Control KY-006 passive piezo buzzer.
+*/
+
+#include "KY006_Buzzer.h"
+
+KY006_Buzzer::KY006_Buzzer(int buzzer_pin) {
+  _buzzerPin = buzzer_pin;
+}
+
+void KY006_Buzzer::begin() {
+}
+
+void KY006_Buzzer::enable() {
+}
+
+void KY006_Buzzer::disable() {
+}
---
+++ KY006_Buzzer/KY006_Buzzer.h
@@ -0,0 +1,26 @@
+/*  Copyright (c) 2021, Collab
+ *  All rights reserved
+*/
+/*
+  KY006_Buzzer.h - Control KY-006 passive piezo buzzer.
+*/
+#ifndef KY006_Buzzer_h
+#define KY006_Buzzer_h
+
+#include <Arduino.h>
+
+class KY006_Buzzer
+{
+  public:
+    KY006_Buzzer(
+      int buzzer_pin
+    );
+    void begin();
+    void enable();
+    void disable();
+
+  private:
+    int _buzzerPin;
+};
+
+#endif
