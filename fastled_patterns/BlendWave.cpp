6f916e251ea8a37afff2b5f2b703fbe10f94d664
Thijs Triemstra <info@collab.nl>
2021-07-27 20:50:13 +0200

    allow to configure palette

--- fastled_patterns/BlendWave.cpp
+++ fastled_patterns/BlendWave.cpp
@@ -10,19 +10,25 @@
 BlendWave::BlendWave(CRGB* neopixels, uint16_t numLEDS) {
   _neopixels = neopixels;
   _numLEDS = numLEDS;
-
-  currentPalette = OceanColors_p;
 }
 
-void BlendWave::loop() {
-  int thisPhase = beatsin8(6,-64,64);                           // Setting phase change for a couple of waves.
-  int thatPhase = beatsin8(7,-64,64);
+void BlendWave::loop(CRGBPalette16 palette) {
+  _currentPalette = palette;
 
-  for (int k=0; k<_numLEDS; k++) {                              // For each of the LED's in the strand, set a brightness based on a wave as follows:
+  // setting phase change for a couple of waves
+  int thisPhase = beatsin8(6, -64, 64);
+  int thatPhase = beatsin8(7, -64, 64);
 
-    int colorIndex = cubicwave8((k*23)+thisPhase)/2 + cos8((k*15)+thatPhase)/2;           // Create a wave and add a phase change and add another wave with its own phase change.. Hey, you can even change the frequencies if you wish.
-    int thisBright = qsuba(colorIndex, beatsin8(7,0,96));                                 // qsub gives it a bit of 'black' dead space by setting sets a minimum value. If colorIndex < current value of beatsin8(), then bright = 0. Otherwise, bright = colorIndex..
+  // for each of the LED's in the strand, set a brightness based on a wave as follows:
+  for (int k=0; k<_numLEDS; k++) {
 
-    _neopixels[k] = ColorFromPalette(currentPalette, colorIndex, thisBright, currentBlending);  // Let's now add the foreground colour.
+    // create a wave and add a phase change and add another wave with its own phase change
+    int colorIndex = cubicwave8((k*23)+thisPhase)/2 + cos8((k*15)+thatPhase)/2;
+    // qsub gives it a bit of 'black' dead space by setting sets a minimum value.
+    // If colorIndex < current value of beatsin8(), then bright = 0. Otherwise, bright = colorIndex..
+    int thisBright = qsuba(colorIndex, beatsin8(7,0,96));
+
+    // Add the foreground colour
+    _neopixels[k] = ColorFromPalette(_currentPalette, colorIndex, thisBright, _currentBlending);
   }
 }
--- fastled_patterns/BlendWave.h
+++ fastled_patterns/BlendWave.h
@@ -8,21 +8,21 @@
 #include <FastLED.h>
 
 // Use qsuba for smooth pixel colouring and qsubd for non-smooth pixel colouring
-#define qsubd(x, b)  ((x>b)?b:0)                              // Digital unsigned subtraction macro. if result <0, then => 0. Otherwise, take on fixed value.
-#define qsuba(x, b)  ((x>b)?x-b:0)                            // Analog Unsigned subtraction macro. if result <0, then => 0
-
+// Digital unsigned subtraction macro. if result <0, then => 0. Otherwise, take on fixed value.
+#define qsubd(x, b)  ((x>b)?b:0)
+// Analog Unsigned subtraction macro. if result <0, then => 0
+#define qsuba(x, b)  ((x>b)?x-b:0)
 
 class BlendWave {
   public:
     BlendWave(CRGB* neopixels, uint16_t numLEDS);
-    void loop();
+    void loop(CRGBPalette16 palette = OceanColors_p);
 
   private:
     CRGB* _neopixels;
     uint16_t _numLEDS;
-    CRGBPalette16 currentPalette;                                 // Palette definitions
-    CRGBPalette16 targetPalette;
-    TBlendType currentBlending = LINEARBLEND;
+    CRGBPalette16 _currentPalette;
+    TBlendType _currentBlending = LINEARBLEND;
 };
 
 #endif
